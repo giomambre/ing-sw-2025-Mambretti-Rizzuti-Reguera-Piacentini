bddbfe0161b5f0e62372a3ca777cff782cadd618
isare <isabel.reguera@mail.polimi.it>
2025-03-11 16:55:59 +0100

    ho scritto ciao

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/App.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "akdlaskldlakdjla COMMIT GIT!" );
+        System.out.println( "ciaoooo COMMIT GIT!" );
     }
 }
