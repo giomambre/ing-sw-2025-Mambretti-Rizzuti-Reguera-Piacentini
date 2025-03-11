e8e8f8fd24127874bc942e9077dfc361cf8ea6f2
raffaelerizzuti <raffaele.rizzuti@mail.polimi.it>
2025-03-11 17:02:35 +0100

    raffa

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/App.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "ciao sono cice COMMIT GIT!" );
+        System.out.println( "ciao000 COMMIT GIT!" );
     }
 }
