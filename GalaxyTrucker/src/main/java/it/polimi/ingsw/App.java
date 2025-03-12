37e32786aed984140d43350d742bef83e6fce5fd
alice <alice.piacentini@mail.polimi.it>
2025-03-12 14:47:04 +0100

    commit di oggi

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/App.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "ciao sono cice COMMIT GIT!" );
+        System.out.println( "ULTIMO COMMIT" );
     }
 }
