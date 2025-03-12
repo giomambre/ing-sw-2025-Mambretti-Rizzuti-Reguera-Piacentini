bf2af8626b89040f83c39d394bb35a2ab45e214b
alice <alice.piacentini@mail.polimi.it>
2025-03-12 16:50:02 +0100

    aggiunto classe game

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/model/Board.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/model/Board.java
@@ -1,4 +1,51 @@
 package it.polimi.ingsw.model;
 
+import java.util.*;
+
 public class Board {
+    private final int BOARD_SIZE = 24;
+    private  Map<Integer, Player> player_position = new HashMap<>();
+    public void MovePlayer(Player p,int pos){ //pos è il numero di pos in aggiunta
+
+        List<Integer> keys = new ArrayList<>(player_position.keySet());
+        keys.sort(Integer::compareTo);
+        int startingPosition = 0;
+        for(var entry : player_position.entrySet()){
+
+            if(entry.getValue().equals(p)){
+                startingPosition = entry.getKey();
+            }
+
+        }
+        for(int i = 0 ; i < pos ; i++){
+
+            System.out.println("da fare");
+
+
+        }
+
+
+
+
+    }
+
+
+    public Map<Integer,Player> GetBoard(){
+        return player_position;
+    }
+    public List<Player> GetRanking(){ //restituisce la lista ordinata
+
+
+        List<Integer> keys = new ArrayList<>( player_position.keySet() );
+        keys.sort(Integer::compareTo);
+        List<Player> ranking = new ArrayList<>();
+        for(Integer i : keys){
+            ranking.add(player_position.get(i));
+
+        }
+    return ranking;
+
+    }
+
 }
+
--- GalaxyTrucker/src/main/java/it/polimi/ingsw/model/ComponentType.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/model/ComponentType.java
@@ -1,13 +1,4 @@
 package it.polimi.ingsw.model;
 
 public enum ComponentType {
-    battery,
-    living_unit,
-    alien_support,
-    storage,
-    cannon,
-    engine,
-    start_cabin,
-    shield,
-    sewer_pipes
 }
