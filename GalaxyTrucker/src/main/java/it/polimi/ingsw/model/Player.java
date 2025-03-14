97bc2830d5a6ee676b0ff3da27ad1fa6237bf689
raffaelerizzuti <raffaele.rizzuti@mail.polimi.it>
2025-03-14 10:35:09 +0100

    metodi ThrowDice e UseExtraComponent

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/model/Player.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model;
 import java.util.List;
+import java.util.Random;
 
 import static  it.polimi.ingsw.model.Color.*;
 
@@ -24,7 +25,6 @@ public class Player {
 
         if (extra_components.size()>1)
             System.out.println("You have more than one extra component");
-
         else
             extra_components.add(component);
 
@@ -38,5 +38,20 @@ public class Player {
         // the player select the box of the ship where he wants to drop the card, controller???
 
         ship.AddComponent(component, row, col);
+    }
+
+    public void UseExtraComponent(CardComponent component) {
+
+        List<CardComponent> extra_components = ship.getExtra_components();
+
+        extra_components.remove(component);
+        ship.setExtra_components(extra_components);
+    }
+
+    public int ThrowDice(){
+        Random dice1 = new Random();
+        Random dice2 = new Random();
+
+        return (dice1.nextInt(6)+1)+(dice2.nextInt(6)+1);
     }
 }
