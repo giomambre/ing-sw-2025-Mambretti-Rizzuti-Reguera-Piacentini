fcf59231840f2c9d97a08c5a66da4ef207227bf2
giomambre <giomambretti77@gmail.com>
2025-03-16 23:04:24 +0100

    Commit di test

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/model/CardAdventure.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/model/CardAdventure.java
@@ -5,13 +5,18 @@ import java.util.List;
 
 public abstract class CardAdventure {
     private int level;
-    private int cost_of_days;
-    private Player current_player;
-
-   //costruttore da vedere se metterlo qui o nelle sottoclassi(classe astratta)
+    private int cost_of_days; //it can be 0
 
 
+   //costruttore da vedere se metterlo qui o nelle sottoclassi(classe astratta) si va fattto
 
+    //da capire come gestire il fatto di chiedere al player se vuole accettare o rifiutare
+    //sono abbastanza sicuro che debba fatto nel controller, ma il controller dovra chiamare qualche metodo
+    // da fare nel player o qui????
+
+    public CardAdventure(int level, int cost_of_days) {}
+
+    public void startAdventure(List <Player> players) {}
     public void nextplayer(){
 
     }
