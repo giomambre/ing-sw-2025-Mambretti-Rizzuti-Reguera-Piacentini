fd10e1d23dd4e864ec4bcbde24d684f098944c50
raffaelerizzuti <raffaele.rizzuti@mail.polimi.it>
2025-03-21 15:27:02 +0100

    bozza AbandonedShip e Planets

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/model/AbandonedShip.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/model/AbandonedShip.java
@@ -1,8 +1,25 @@
 package it.polimi.ingsw.model;
 
-public class AbandonedShip {
+import java.util.List;
+
+public class AbandonedShip extends CardAdventure {
     int given_credits;
     int crewmates_loss;
 
-    public AbandonedShip(int given_credits, int crewmates_loss) {}
+    public AbandonedShip(int level, int cost_of_days, CardAdventureType type, Board board, int given_credits, int crewmates_loss) {
+        super(level, cost_of_days,type ,board);
+        this.given_credits = given_credits;
+        this.crewmates_loss = crewmates_loss;
+    }
+
+
+    public void removeCrewmates(Player player, List<CardComponent[][]> living_unit_positions) {
+
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
 }
---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/model/Planets.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Planets extends CardAdventure{
+    List<List<Cargo>> cargo_reward;
+
+    public Planets(int level, int cost_of_days, CardAdventureType type, Board board, List<List<Cargo>> cargo_reward) {
+        super(level, cost_of_days,type ,board);
+        this.cargo_reward = cargo_reward;
+    }
+
+    public List<Cargo> givePlayerRewards(Player player, int num_planet) {
+        return cargo_reward.get(num_planet);
+    }
+    @Override
+    public void execute() {
+
+    }
+}
