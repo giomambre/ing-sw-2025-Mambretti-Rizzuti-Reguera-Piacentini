582ce37e9175f15557087b7bf2b2bb67eeaa75fb
raffaelerizzuti <raffaele.rizzuti@mail.polimi.it>
2025-03-24 14:31:15 +0100

    Aggiunto metodi findPinkAlien e findBrownAlien e modificato calculate engine e cannon power

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/model/LivingUnit.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/model/LivingUnit.java
@@ -5,27 +5,27 @@ import java.util.Map;
 public class LivingUnit extends CardComponent{
 
     private int num_astronaut;
-    private CrewmateType crewate_type;
+    private CrewmateType crewmate_type;
 
     public LivingUnit(ComponentType component_type, Map<Direction, ConnectorType> connectors) {
         super(component_type, connectors);
         this.num_astronaut = 0;
     }
 
-    public void addCrewmate(CrewmateType crewate_type) {
-    switch (crewate_type) {
+    public void addCrewmate(CrewmateType crewmate_type) {
+    switch (crewmate_type) {
         case Astronaut:
             this.num_astronaut=2;
-            this.crewate_type=crewate_type;
+            this.crewmate_type=crewmate_type;
             break;
         case PinkAlien:
             this.num_astronaut=1;
-            this.crewate_type=crewate_type;
+            this.crewmate_type=crewmate_type;
             break;
 
         case BrownAlien:
             this.num_astronaut=1;
-            this.crewate_type=crewate_type;
+            this.crewmate_type=crewmate_type;
             break;
     }
     }
@@ -34,15 +34,15 @@ public class LivingUnit extends CardComponent{
         return num_astronaut;
     }
 
-    public CrewmateType getCrewate_type() {
-        return crewate_type;
+    public CrewmateType getCrewmateType() {
+        return crewmate_type;
     }
 
     public void RemoveCrewmates(int num_to_remove){
 
         num_astronaut -= num_to_remove;
 
-        if(num_astronaut == 0) crewate_type=CrewmateType.None;
+        if(num_astronaut == 0) crewmate_type=CrewmateType.None;
 
 
     }
