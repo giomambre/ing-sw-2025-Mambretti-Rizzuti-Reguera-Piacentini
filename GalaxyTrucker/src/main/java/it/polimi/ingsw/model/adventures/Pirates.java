ef6295049085fbd90091f9bd541ca352c738c6b4
raffaelerizzuti <raffaele.rizzuti@mail.polimi.it>
2025-04-04 09:08:23 +0200

    finiti test cardAdventure e scritti e testati slayers e pirates

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/model/adventures/Pirates.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.model.adventures;
+
+import it.polimi.ingsw.model.Board;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.enumerates.CardAdventureType;
+import it.polimi.ingsw.model.enumerates.Direction;
+import it.polimi.ingsw.model.enumerates.MeteorType;
+import javafx.util.Pair;
+
+import java.util.List;
+
+public class Pirates extends CardAdventure{
+    private int cannons_strenght;
+    private int credits;
+    List<Pair<MeteorType, Direction>> meteors;
+
+    public Pirates(int level, int cost_of_days, CardAdventureType type, Board board, int cannons_strenght, int credits, List<Pair<MeteorType, Direction>> meteors ) {
+        super(level, cost_of_days, type, board);
+        this.cannons_strenght = cannons_strenght;
+        this.credits=credits;
+        this.meteors = meteors;
+    }
+
+    public void executeWin(Player player) {
+        board.movePlayer(player, -getCost_of_days());
+        player.receiveCredits(credits);
+    }
+
+    //executeloss gestito da meteorswarm
+    public List<Pair<MeteorType, Direction>> getMeteors() {
+        return meteors;
+    }
+}
