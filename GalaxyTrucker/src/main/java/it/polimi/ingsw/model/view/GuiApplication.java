1a9d00d4e461ced7093ae044360fdfeedec5012f
isare <isabel.reguera@mail.polimi.it>
2025-04-28 22:17:38 +0200

    continuo GUI

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/model/view/GuiApplication.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.model.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+
+public class GuiApplication extends Application {
+    public void start(Stage primaryStage) {
+
+    }
+}
